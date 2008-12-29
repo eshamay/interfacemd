1f82790488b041a84c51624cba22664586074b2e
eric <eshamay@uoregon.edu>
2008-12-29 14:57:19 -0800

    fixed up final interface-averaging

--- test/orderparams.h
+++ test/orderparams.h
@@ -8,14 +8,11 @@
 #define MDCRD	"mdcrd"
 #define FORCE	""
 
-#define INT_LOW		29.771
-#define	INT_HIGH	74.2
-
 class OrderParameters {
 
 public:
 
-	OrderParameters ();
+	OrderParameters (int argc, char **argv);
 
 	AmberSystem * sys;
 
@@ -49,47 +46,5 @@ public:
 	void PrintStatus ();
 };
 
-OrderParameters::OrderParameters () {
-	
-	// here is our system for analysis
-	sys = new AmberSystem (PRMTOP, MDCRD, FORCE);
-
-	output = (FILE *)NULL;
-	output = fopen ("orderparams.dat", "w");
-	if (output == (FILE *)NULL) {
-		printf ("couldn't open the output file for reading!\n");
-		exit (1);
-	}
-
-	axis = y;
-	
-	output_freq	=	10;					// how often the output file will be written (# of timesteps/10)
-	timesteps	=	200000;				// # of timesteps to process through
-	
-	// position boundaries and bin width
-	posmin	= -25.0;
-	posmax	= 25.0;
-	posres	= 0.5;
-	posbins = (posmax-posmin)/posres;
-
-	int_low = INT_LOW;
-	int_high = INT_HIGH;
-	middle = (int_low + int_high)/2.0;
-	
-	angmax	 = 1.0;
-	angmin	 = -1.0;
-	angres	 = 0.05;
-	angbins = (angmax-angmin)/angres;
-	
-	// setup and initialize the system histogram
-	// The histo looks like histo[y-position][S1][S2 numerator][S2 denominator]
-	S1.clear(); S1.resize (posbins, 0.0);
-	S2_num.clear(); S2_num.resize (posbins, 0.0);
-	S2_den.clear(); S2_den.resize (posbins, 0.0);
-	number_density.clear(); number_density.resize(posbins, 0);
-
-return;
-}
-	
 
 #endif
